0f3e0cf939b5788faa16e703adc30f903cc487fe
AleClo <acloutier@acloutier.com>
2018-02-06 16:46:03 -0500

    Adding comment to provoke conflict

--- LaboNetBeanGit/src/labonetbeangit/LaboNetBeanGit.java
+++ LaboNetBeanGit/src/labonetbeangit/LaboNetBeanGit.java
@@ -16,6 +16,7 @@ public class LaboNetBeanGit {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        // Adding a comment on line 19
     }
     
 }
